a549e3762242bc55705bde9f20e764ca003c7ca8
Rahul Onkar Kale <rahulonkarkale@Rahuls-MacBook-Air.local>
2025-10-03 23:01:21 +0530

    added integration test

--- application-service/src/test/java/com/k8s/application_service/ApplicationServiceApplicationTests.java
+++ application-service/src/test/java/com/k8s/application_service/ApplicationServiceApplicationTests.java
@@ -1,13 +1,41 @@
 package com.k8s.application_service;
 
+import com.k8s.common.dto.ApplicationDTO;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.event.annotation.BeforeTestClass;
+import org.springframework.util.Assert;
+import org.springframework.web.client.RestTemplate;
+import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest
+@SpringBootTest(properties = "spring.profiles.active=test")
 class ApplicationServiceApplicationTests {
 
+	@Autowired
+	RestTemplate restTemplate;
+
+	@BeforeTestClass
+	public void init(){
+		this.restTemplate = new RestTemplate();
+	}
+
+	@Value("${app.url}")
+	private String appUrl;
+
 	@Test
-	void contextLoads() {
+	public void test_CreateApplication(){
+		ApplicationDTO application = new ApplicationDTO();
+		application.setAppDisplayId("Test App Display");
+		application.setAppName("Test App");
+		application.setDeploymentId("TEST-2323");
+		application.setOwnerEmail("test@gmail.com");
+		ResponseEntity result = restTemplate.postForEntity(appUrl+"/api/v1/apps/", application, Object.class);
+		assertEquals(HttpStatusCode.valueOf(200), result.getStatusCode());
 	}
 
 }
